8c9790fae739cd511ff725f1e40a8f92afd391a8
schneediener <100398434+schneediener@users.noreply.github.com>
2022-03-05 01:18:39 +1030

    add pause menu
    replaced R reset option with full pause menu, including pause/resume on esc, restart button, and exit

---
+++ PauseMenu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+var is_paused = false setget set_is_paused
+
+func _ready():
+	visible = false
+
+func _unhandled_input(event):
+		if event.is_action_pressed("ui_cancel"):
+			self.is_paused = !is_paused
+			visible = is_paused
+
+func set_is_paused(value):
+	is_paused = value
+	get_tree().paused = is_paused
+	visible = is_paused
+
+func _on_ResumeButton_pressed():
+	self.is_paused = false
+
+func _on_RestartButton_pressed():
+	get_tree().reload_current_scene()
+	get_tree().paused = false
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
